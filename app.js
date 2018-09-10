5b74ea92d58d838e3be0881675a44f419393a87e
nokranger <anusorn.thavornporn@gmail.com>
2018-09-10 21:47:43 +0700

    app.get student and query

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ const mongodb = require('mongodb')
 
 var app = express()
 
+let student = [
+    {
+        id:'st1',name:'aaa'
+    },
+    {
+        id:'st2',name:'bbb'
+    },
+    {
+        id:'st3',name:'ccc'
+    }
+]
 app.get('/',(req,res)=>{
     res.send('Hello')
+})
+
+app.get('/all-Student',(req,res)=>{
+    res.send(student)
+})
+app.get('/all-Student/:id',(req,res)=>{
+    let studentID = req.params.id
+    for(let i=0;i<student.length;i++){
+        if(studentID == student[i].id){
+            res.send(student[i])
+            break
+        }
+    }
+    res.send('not found this id : '+ studentID)
 })
 
 app.listen(3000,()=>{
